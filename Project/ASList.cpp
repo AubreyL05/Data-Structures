302eaf7b1f368354ccebcd62f4829d4b3fdf8130
luke <lukeaaubrey@gmail.com>
2025-10-17 17:38:26 -0400

    Initial implementation of functions, working

---
+++ Project/ASList.cpp
@@ -0,0 +1,42 @@
+#include "ASList.h"
+#include <iostream>
+
+ASList::ASList() {
+    length = 0;
+}
+
+void ASList::insert(const Patient& p) {
+    int i = length - 1;
+    while (i >= 0 && !(data[i] > p)) {
+        data[i + 1] = data[i];
+        i--;
+    }
+    data[i + 1] = p;
+    length++;
+}
+
+void ASList::print() const {
+    for (int i = 0; i < length; i++){
+        cout << "("
+             << data[i].patientID << ", "
+             << data[i].age << ", "
+             << data[i].procedure << ", "
+             << data[i].cost << ", "
+             << data[i].length_of_stay
+             << ")" << endl;
+    }
+}
+
+void ASList::printFiltered(int days) const {
+    for (int i = 0; i < length; i++) {
+        if (data[i].length_of_stay >= days) {
+            cout << "("
+                 << data[i].patientID << ", "
+                 << data[i].age << ", "
+                 << data[i].procedure << ", "
+                 << data[i].cost << ", "
+                 << data[i].length_of_stay
+                 << ")" << endl;
+        }
+    }
+}
